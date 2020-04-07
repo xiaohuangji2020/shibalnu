d04aeef5293207bc3a6617176b40b514c9ae1983
roco2015 <roco2015@163.com>
2020-04-07 16:42:31 +0800

    format

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,33 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+      node: true
   },
   extends: [
-    "plugin:vue/essential",
-    "eslint:recommended",
-    "@vue/typescript/recommended",
-    "@vue/prettier",
-    "@vue/prettier/@typescript-eslint"
+      "plugin:vue/essential",
+      "eslint:recommended",
+      "@vue/typescript/recommended",
+      "@vue/prettier",
+      "@vue/prettier/@typescript-eslint",
   ],
   parserOptions: {
-    ecmaVersion: 2020
+      ecmaVersion: 2020
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+      "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+      "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+      "prettier/prettier": "error",
+      "max-len": ["error", { "code": 120, "tabWidth": 4 }]
   },
   overrides: [
-    {
-      files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)"
-      ],
-      env: {
-        jest: true
+      {
+          files: [
+              "**/__tests__/*.{j,t}s?(x)",
+              "**/tests/unit/**/*.spec.{j,t}s?(x)"
+          ],
+          env: {
+              jest: true
+          }
       }
-    }
   ]
 };
