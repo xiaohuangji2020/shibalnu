b1dec0705c576ea9185337338ee1a13add45f690
枫叶传说 <roco2015@163.com>
2020-04-08 23:56:09 +0800

    首页

---
+++ src/components/common/header.vue
@@ -0,0 +1,38 @@
+<template>
+    <header class="header fixed">
+        <div class="logo">shibalnu</div>
+        <my-nav></my-nav>
+    </header>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import myNav from '@/components/common/nav.vue';
+
+@Component({
+    components: {
+        myNav
+    }
+})
+export default class Header extends Vue {}
+</script>
+
+<style lang="less" scoped>
+.header {
+    display: flex;
+    width: 100%;
+    height: 100px;
+    line-height: 100px;
+
+    &.fixed {
+        position: fixed;
+        z-index: 50;
+        top: 0;
+        background: white;
+        box-shadow: 0 8px 16px 0 rgba(64, 75, 91, 0.1);
+    }
+    .logo {
+        width: 100px;
+    }
+}
+</style>
--- src/views/index.vue
+++ src/views/index.vue
@@ -1,19 +1,19 @@
 <template>
     <div class="index">
-        <header class="header">
-            <div class="logo">shibalnu</div>
-            <my-nav></my-nav>
-        </header>
+        <my-header></my-header>
+        <div class="bar">
+            <span class="brand">北方优柴</span>
+        </div>
     </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import myNav from '@/components/common/nav.vue';
+import myHeader from '@/components/common/header.vue';
 
 @Component({
     components: {
-        myNav
+        myHeader
     }
 })
 export default class Index extends Vue {}
@@ -21,16 +21,29 @@ export default class Index extends Vue {}
 
 <style lang="less" scoped>
 .index {
-    height: 100vh;
     position: relative;
+    height: 200vh;
+    border-top: 1px solid transparent;
     background: @bg-light;
 }
-.header {
-    display: flex;
-    height: 100px;
-    line-height: 100px;
-    .logo {
-        width: 100px;
+.bar {
+    position: relative;
+    z-index: 30;
+    height: 300px;
+    line-height: 300px;
+    margin-top: calc(~'100vh - 300px');
+    background: url('~@/assets/images/bg/bg-4.png') no-repeat;
+    background-size: cover;
+    background-attachment: fixed;
+
+    .brand {
+        display: block;
+        color: white;
+        font-size: 64px;
+        font-weight: 500;
+        letter-spacing: 25px;
+        filter: drop-shadow(0 0 25px rgba(238, 77, 45, 0.8));
+        text-shadow: rgba(238, 77, 45, 0.8) 0 0 10px;
     }
 }
 </style>
