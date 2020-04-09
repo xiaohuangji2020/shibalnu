f69c519de43a3c000725f3d97872fec1252b6ac5
roco2015 <roco2015@163.com>
2020-04-09 16:42:43 +0800

    提交下logo

--- src/components/common/header.vue
+++ src/components/common/header.vue
@@ -1,17 +1,19 @@
 <template>
     <header class="header fixed">
-        <div class="logo">shibalnu</div>
+        <logo></logo>
         <my-nav></my-nav>
     </header>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import logo from '@/components/common/logo.vue';
 import myNav from '@/components/common/nav.vue';
 
 @Component({
     components: {
-        myNav
+        myNav,
+        logo
     }
 })
 export default class Header extends Vue {}
@@ -30,9 +32,6 @@ export default class Header extends Vue {}
         top: 0;
         background: white;
         box-shadow: 0 8px 16px 0 rgba(64, 75, 91, 0.1);
-    }
-    .logo {
-        width: 100px;
     }
 }
 </style>
---
+++ src/components/common/logo.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="logo">
+        <span class="t-1">北方</span>
+        <span class="t-2">优柴</span>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Logo extends Vue {}
+</script>
+
+<style lang="less" scoped>
+.logo {
+    position: relative;
+    width: 120px;
+    background: url('~@/assets/images/shibalnu/logo-4.jpg') no-repeat;
+    background-size: cover;
+    background-position: center;
+
+    span {
+        position: absolute;
+        line-height: 1;
+        animation: tada 10s infinite;
+    }
+
+    .t-1 {
+        bottom: 0;
+        left: 0;
+        animation-delay: 5s;
+    }
+
+    .t-2 {
+        right: 0;
+        bottom: 0;
+        animation-delay: 7s;
+    }
+}
+</style>
